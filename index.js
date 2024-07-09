c4cd6b1f3bddfd8870fe351482c11fc585f72eb0
Elvis Sonatha <elviskudo@hotmail.com>
2024-07-09 16:54:12 +0700

    add authorization

--- index.js
+++ index.js
@@ -3,36 +3,49 @@ const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
+require('dotenv').config;
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*', // Atur origin sesuai kebutuhan
-    methods: ['GET', 'POST']
+    origin: process.env.FRONTEND_URL, // Ganti dengan URL frontend Anda jika di-deploy
+    methods: ["GET", "POST"]
   }
 });
 
-app.use(cors()); // Tambahkan middleware CORS
+app.use(cors());
 
-let clients = [];
+// Middleware untuk otorisasi token
+const AUTH_TOKEN = process.env.AUTH_TOKEN
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  // Lakukan verifikasi token disini
+  if (token === AUTH_TOKEN) { // Ganti dengan logika verifikasi token yang sesuai
+    next();
+  } else {
+    next(new Error("Unauthorized"));
+  }
+});
 
 io.on('connection', (socket) => {
-  clients.push(socket);
+  console.log('a user connected');
 
+  // Contoh penanganan pesan dari klien
   socket.on('chat message', (msg) => {
-    clients.forEach(client => {
-      if (client !== socket) {
-        client.emit('chat message', msg);
-      }
-    });
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
-    clients = clients.filter(client => client !== socket);
+    console.log('user disconnected');
   });
 });
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
